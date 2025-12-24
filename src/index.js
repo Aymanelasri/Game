86734856ef0ea9ff262219e30cf673eea60dee0f
Aymane <aymanelasri100@gmail.com>
2025-12-24 23:34:52 +0100

    Fix index.js imports for Netlify build

--- src/index.js
+++ src/index.js
@@ -2,30 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-
+import Game from './Game/game';
 import reportWebVitals from './reportWebVitals';
-
-import UsersApp from './UserManagement/UsersApp';
-import Ttt from './Exercice/Ttt';
-import Projets from './ProjetsFinal/Projets';
-import ThreeDAutoRotate from './Events/Event'
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Router /> */}
-     {/*<App /> */}
-    {/*} <Form />*/}
-    {/*<Main />*/}
-    {/*<UsersApp />*/}
-    {/*<Ttt />*/}
-    <Projets />
-    
-    {/* <Exercice />} */}
+    <Game/>
   </React.StrictMode>
 );
 
